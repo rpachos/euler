605f719341629c8ef9d6e359c2dcb1f0958f24b7
Robert  Pachos <rob.pachos@gmail.com>
2015-04-03 22:26:04 -0400

    solved problem 20 by monkey-patching Integer!

---
+++ 20.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+#Factorial digit sum
+#Problem 20
+#n! means n × (n − 1) × ... × 3 × 2 × 1
+#
+#For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+#and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+#Find the sum of the digits in the number 100!
+
+class Integer
+  def factorial
+    if self == 1
+      return self
+    else
+      return self * (self - 1).factorial
+    end
+  end
+end
+
+puts 100.factorial.to_s.chars.map {|digit| digit }.map {|char| char.to_i }.inject {|digit,sum| digit + sum }
